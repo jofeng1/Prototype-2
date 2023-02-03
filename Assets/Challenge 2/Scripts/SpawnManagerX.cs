86b6603b7e431176d3074b8a27063db51385cf23
jofeng1 <122054405+jofeng1@users.noreply.github.com>
2023-02-03 12:09:32 -0800

    Finished Challenge 2

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -25,8 +25,12 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        //instantiate random ball from array
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        //random spawnInterval
+        spawnInterval = Random.Range(0.5f, 3.0f);
     }
 
 }
